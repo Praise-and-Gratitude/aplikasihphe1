42d1fd8b2312d0203d4e61553ffd83ed4bc27c53
Ardi Rakhmat <65388076+Praise-and-Gratitude@users.noreply.github.com>
2022-02-24 11:01:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 function calculate2() 
 /* Input spesifikasi Dimensi dan Spesifikasi HPHE*/
 {   let baris=document.getElementById("baris").value
@@ -28,7 +23,7 @@ function calculate2()
 /* End of Perhitungan L2 dan L3 */
 
 
-/* Input Kondisi Operasional ; Line 350 HTML*/
+/* Input Kondisi Operasional ;*/
 let seleect1 = document.getElementById('jumlahmodul2');
 let seleect2 = document.getElementById('tei');
 let seleect3 = document.getElementById('vei');
@@ -37,9 +32,7 @@ let seleect3 = document.getElementById('vei');
 const chooice1  = seleect1.value
 const chooice2 = seleect2.value;
 const chooice3 = seleect3.value;
-
-
-  if (chooice1 === '11' && chooice2 === '30' && chooice3 === '15') {
+if (chooice1 === '11' && chooice2 === '30' && chooice3 === '15') {
      tes10 = 80.4
     } else if (chooice1 === '11' && chooice2 === '30' && chooice3 === '20') {
       tes10 = 114.1
@@ -112,143 +105,69 @@ const chooice3 = seleect3.value;
        } else if (chooice1 === '33' && chooice2 === '45' && chooice3 === '25') {
            tes10 = 870.7
        }
-
 /*End of Penentuan heat recovery berdasarkan jumlah baris, temperatur dan kecepatan memasuki evaporator */
 
 let dailyuse2 = document.getElementById("dailyuse").value;
 let monthlyuse2 = document.getElementById("monthlyuse").value;
 let biayalistrik= document.getElementById("biayalistrik").value;
 
-
 /* End of Input Kondisi Operasional */
 
-
-    
-    
 /*Perhitungan biaya investasi HPHE */  
     let dudukan = 2 *lebar * kedalaman * 3/1000000000* 777796794
     let rangka = 4*(+adiab + +kond)*30*20/1000000000 *47709091
-
     let rawgalvalum = +rangka + +dudukan
-
     let rawvfin = (+evap + +kond) *lebar *kedalaman*2/selllect*843/100000
-
-    
-
     let galvalum =new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format( Math.round(+rangka + +dudukan))
-   
     let heatpipe =new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(Math.floor(bariss*kolom*baris))
-
-    
-    
-
     let vfin = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format((Math.round((+evap + +kond) *lebar *kedalaman*2/selllect*843/100000)));
-
     let produksi =  new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format( Math.round(3*(+rawgalvalum + +rawvfin)))
-
     let instalasi = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(250000)
-
     let benefitmargin1= 3*(+rawgalvalum + +rawvfin)
-
     let benefitmargin2 = bariss*kolom*baris
-
     let benefitmargin3= 250000
-
     let benefitmargin = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(Math.round((+benefitmargin1 + +benefitmargin2 + +benefitmargin3)/4))
-    
     const biayatotal = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(Math.round((+benefitmargin1 + +benefitmargin2 + +benefitmargin3)*5/4))
     let rawbiayatotal= (+benefitmargin1 + +benefitmargin2 + +benefitmargin3)*5/4
     const biayatotal2 = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(Math.round((+benefitmargin1 + +benefitmargin2 + +benefitmargin3)*5/4*2/3))
     const biayatotal3 = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(Math.round((+benefitmargin1 + +benefitmargin2 + +benefitmargin3)*5/4*4/3))
     let rawbiayatotal2 = Math.round((+benefitmargin1 + +benefitmargin2 + +benefitmargin3)*5/4*2/3)
     let rawbiayatotal3 = Math.round((+benefitmargin1 + +benefitmargin2 + +benefitmargin3)*5/4*4/3)
-    
-    
-    
-    
     document.getElementById('tesst2').innerHTML = vfin
-    
     document.getElementById('tesst3').innerHTML = galvalum
-
     document.getElementById('tesst4').innerHTML = heatpipe
-
     document.getElementById('tesst5').innerHTML = produksi
-
     document.getElementById('tesst7').innerHTML = instalasi
-
     document.getElementById('tesst8').innerHTML = benefitmargin
-
     document.getElementById('tesst9').innerHTML = biayatotal
-
     document.getElementById('tesst9x').innerHTML = biayatotal2
-
     document.getElementById('tesst9xx').innerHTML = biayatotal3
-
 /* End of perhitungan biaya investasi HPHE */  
    
-    
-    
-
-
-
-
-    
-	
-
 /*Perhitungan Penghematan Listrik */
-
 const pengembalian = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(tes10*dailyuse2*monthlyuse2*4*biayalistrik/1000)
 let rawpengembalian= tes10*dailyuse2*monthlyuse2*4*biayalistrik/1000
-
 /*End of Perhitungan Penghematan Listrik */
 
-        
-
-    
-
-  
-  
-
 /*Perhitungan Payback Period */
-
 document.getElementById('testes10').innerHTML = tes10
-    
 document.getElementById('testes13').innerHTML = pengembalian
-
-
-
 document.getElementById('newpembagian').innerHTML= Math.trunc(Math.round(rawbiayatotal)/Math.round(rawpengembalian))
-
 document.getElementById('sisapembagian2').innerHTML= Math.trunc((rawbiayatotal%rawpengembalian)/Math.round(rawpengembalian)*12)
-
 document.getElementById('newpembagian3').innerHTML= Math.trunc(Math.round(rawbiayatotal2)/Math.round(rawpengembalian))
-
 document.getElementById('sisapembagian3').innerHTML= Math.trunc((rawbiayatotal2%rawpengembalian)/Math.round(rawpengembalian)*12)
-
 document.getElementById('newpembagian4').innerHTML= Math.trunc(Math.round(rawbiayatotal3)/Math.round(rawpengembalian))
-  
 document.getElementById('sisapembagian4').innerHTML= Math.trunc((rawbiayatotal3%rawpengembalian)/Math.round(rawpengembalian)*12)
-
+/*End of Perhitungan Payback Period */
 }
 
-/*End of Perhitungan Payback Period */
-
 /*Pengubahan Tombol 'Hitung' menjadi 'Hitung Lagi' */
-  let button = document.getElementById('button3');
-  let visible= document.getElementById('invisible')
-  
+let button = document.getElementById('button3');
+let visible= document.getElementById('invisible')
   function changebutton () {
-      
-          button.innerHTML= 'Hitung Lagi'
-          invisible.style.display = 'block';
-      
-      
-      
-      
-      
-  }
-      
-  
-  button.addEventListener('click', changebutton)
-  /*End of Pengubahan Tombol 'Hitung' menjadi 'Hitung Lagi' */
+    button.innerHTML= 'Hitung Lagi'
+    invisible.style.display = 'block';
+    }
+button.addEventListener('click', changebutton)
+/*End of Pengubahan Tombol 'Hitung' menjadi 'Hitung Lagi' */
   
